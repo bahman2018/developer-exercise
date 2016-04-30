c81c4a0e6b7eecd24b0ddd12f7a885406288f080
Bahman <info@bahman.ws>
2016-04-30 16:06:12 -0700

    4-30-2016

--- exercise.rb
+++ exercise.rb
@@ -5,6 +5,24 @@ class Exercise
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
     # TODO: Implement this method
+	  j=i=0
+	  str.strip!
+    begin
+      if str[i]==" " || i==str.size
+         if (i-j)>4
+           temp1=if j>0 then str[0..j-1] else "" end
+           temp2=if str[i-1]== "?" or str[i-1]=="." then str[i-1] else "" end
+           if(str[j].upcase==str[j])
+             str=temp1+"Marklar"+temp2+str[i..str.size]
+           else
+             str=temp1 + "marklar"+temp2+str[i..str.size]
+           end
+         end
+         j=i+1
+      end
+      i+=1
+    end while i<=str.size
+    return str
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
@@ -13,6 +31,21 @@ class Exercise
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
     # TODO: Implement this method
+    a = 0
+		b = 1
+		sum=0
+
+		# Compute Fibonacci number in the desired position.
+		nth.times do
+      temp = a
+      a = b
+      # Add up previous two numbers in sequence.
+      b = temp + b
+      sum+= if a%2==0 then a else 0 end
+		end
+
+    return sum
+
   end
 
 end
